2d6d31bf85b34d6d340741262d99272750a78d51
Michael Malchak <michaelmalchak@gmail.com>
2014-12-28 19:33:07 -0800

    Complete chapter 5: computed properties and observers

--- app/models/friend.js
+++ app/models/friend.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data';
 import Ember from 'ember';
+import changeGate from 'ember-computed-change-gate/change-gate';
 
 export default DS.Model.extend({
   articles: DS.hasMany('article', { async: true }),
@@ -8,6 +9,9 @@ export default DS.Model.extend({
   email: DS.attr('string'),
   twitter: DS.attr('string'),
   totalArticles: DS.attr('number'),
+  capitalizedFirstName: changeGate('firstName', function (firstName) {
+    return Ember.String.capitalize(firstName);
+  }),
   fullName: Ember.computed('firstName', 'lastName', function () {
     return this.get('firstName') + ' ' + this.get('lastName');
   })
